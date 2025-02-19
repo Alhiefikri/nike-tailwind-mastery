533498b4c79de8086c5c3a4d94cc5676d3920373
alhiefikri <alhiepedia@gmail.com>
2025-02-19 11:01:29 +0800

    feat: adding  SuperQuality section

--- src/sections/SuperQuality.jsx
+++ src/sections/SuperQuality.jsx
@@ -1,4 +1,42 @@
+import { shoe8 } from "../assets/images";
+import Button from "../components/Button";
+
 const SuperQuality = () => {
-  return <div>SuperQuality</div>;
+  return (
+    <section
+      id="about-us"
+      className="flex justify-between items-center max-lg:flex-col gap-10 w-full max-container"
+    >
+      <div className="flex flex-1 flex-col">
+        <h2 className="font-palanquin text-4xl capitalize font-bold lg:max-w-lg">
+          We Provide You <span></span>
+          <span className="text-coral-red inline-block"> Super</span>
+          <span className="text-coral-red inline-block">Quality</span> Shoes
+        </h2>
+        <p className="mt-4 lg:max-w-lg info-text">
+          Ensuring premium comfort and style, our meticulously crafted footwear
+          combines cutting-edge technology with timeless design to deliver
+          unparalleled performance and elegance.
+        </p>
+
+        <p className="mt-6 lg:max-w-lg info-text">
+          Our dedication to detail and excellence ensures your satisfaction
+        </p>
+        <div className="mt-11">
+          <Button>View details</Button>
+        </div>
+      </div>
+
+      <div className="flex-1 flex justify-center items-center">
+        <img
+          src={shoe8}
+          alt="shoe8"
+          width={570}
+          height={522}
+          className="object-contain"
+        />
+      </div>
+    </section>
+  );
 };
 export default SuperQuality;
